ccdb3a62f5bf14352cbdd1b87412e744e05de451
Owen Plunkett <oplunkett@lcs.on.ca>
2022-02-02 18:58:27 -0500

    Finished the main bit of my code, i just need to add the text.

--- Playground.playground/Pages/Empty Canvas 10.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Empty Canvas 10.xcplaygroundpage/Contents.swift
@@ -29,21 +29,6 @@ let p = Pen(drawingUpon: canvas)
 // Show the canvas in the playground's live view
 PlaygroundPage.current.liveView = canvas
 
-/*:
- ## Optional code
- 
- Below are two generally helpful configurations.
- 
- If you do not wish to work in all four quadrants of the Cartesian plane, comment out the code on line 44.
- 
- If you do not wish to see a grid, comment out the code on line 48.
- 
- */
-
-
-
-canvas.fillColor = .black
-canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 600)
 
 /*:
  ## Add your code
@@ -53,83 +38,51 @@ canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 600)
  [Documentation](http://russellgordon.ca/CanvasGraphics/Documentation/) is available.
  
  */
-canvas.highPerformance = true
+canvas.highPerformance = false
 
-//Create drak blue colour
+// Black background
+canvas.fillColor = .black
+canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 600)
+
+//Create dark blue colour
 let darkBlue = Color(hue: 240, saturation: 100, brightness: 55, alpha: 100)
-//
-////gradient for window
-//for value in stride(from: 0, through: 480, by: 1) {
-//
-//    canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 600)
-//
-//    // Change the "value" to a value in a different range
-//    let currentHue = map(value: Double(value), fromLower: 0.0, fromUpper: 480.0, toLower: 0.0, toUpper: 60.0)
-//
-//    // Create a new color
-//    let currentColor = Color(hue: Int(currentHue), saturation: 80, brightness: 90, alpha: 100)
-//
-//    // Set the line color
-//    canvas.lineColor = currentColor
-//
-//
-//}
-
-
-
-//for _ in stride(from: 0, through: 600, by: 200){
-//    for _ in stride(from: 0, through: 400, by: 100){
-//
-//
-//    }
-//}
-// Draw left row of Triangles (left to right)
-// Repete of 50 and 0 in from line of code
-canvas.drawLine(from: Point(x: 0, y: 0), to: Point(x: 50, y: 100))
-canvas.drawLine(from: Point(x: 50, y: 100), to: Point(x: 100, y: 0))
-canvas.drawLine(from: Point(x: 0, y: 200), to: Point(x: 50, y: 100))
-canvas.drawLine(from: Point(x: 50, y: 100), to: Point(x: 100, y: 200))
-canvas.drawLine(from: Point(x: 0, y: 200), to: Point(x: 50, y: 300))
-canvas.drawLine(from: Point(x: 50, y: 300), to: Point(x: 100, y: 200))
-canvas.drawLine(from: Point(x:0, y: 400), to: Point(x: 50, y: 300))
-canvas.drawLine(from: Point(x: 50, y: 300), to: Point(x: 100, y: 400))
-canvas.drawLine(from: Point(x: 0, y: 400), to: Point(x: 100, y: 400))
-
-
-// Draw second row of Triangles
-
-canvas.drawLine(from: Point(x: 150, y: 100), to: Point(x: 100, y: 0))
-canvas.drawLine(from: Point(x: 200, y: 0), to: Point(x: 150, y: 100))
-canvas.drawLine(from: Point(x: 100, y: 200), to: Point(x: 150, y: 100))
-canvas.drawLine(from: Point(x: 150, y: 100), to: Point(x: 200, y: 200))
-canvas.drawLine(from: Point(x: 150, y: 300), to: Point(x: 200, y: 200))
-canvas.drawLine(from: Point(x: 100, y: 200), to: Point(x: 150, y: 300))
-canvas.drawLine(from: Point(x: 100, y: 400), to: Point(x: 150, y: 300))
-canvas.drawLine(from: Point(x: 150, y: 300), to: Point(x: 200, y: 400))
-
-
-
-//Draw third line of Triangles
-
-
-
-
-
-
-
-
 
 // Draw top line
-
 canvas.drawLine(from: Point(x: 0, y: 400), to: Point(x: 400, y: 400))
 
+// Draw first set of triangles
+canvas.drawShapesWithFill = true
+canvas.fillColor = darkBlue
 
+for xpositionTriangle1 in stride(from: 0, through: 400, by: 100) {
+    for ypositionTriangle1 in stride(from: 100, through: 400, by: 200) {
+        
+        var Triangle1: [Point] = []
+        Triangle1.append(Point(x: xpositionTriangle1 + 50, y: ypositionTriangle1 + 0))
+        Triangle1.append(Point(x: xpositionTriangle1 + 0, y: ypositionTriangle1 + 100))
+        Triangle1.append(Point(x: xpositionTriangle1 + 0 + 100, y: ypositionTriangle1 + 100))
+        canvas.drawCustomShape(with: Triangle1)
+        
+    }
+}
 
+// Draw second set of triangles
 
+for xpositionTriangle2 in stride(from: 0, through: 400, by: 100) {
+    for ypositionTriangle2 in stride(from: 0, through: 300, by: 200) {
+        
+        var Triangle2: [Point] = []
+        Triangle2.append(Point(x: xpositionTriangle2 + 0, y: ypositionTriangle2 + 0))
+        Triangle2.append(Point(x: xpositionTriangle2 + 50, y: ypositionTriangle2 + 100))
+        Triangle2.append(Point(x: xpositionTriangle2 + 100, y: ypositionTriangle2 + 0))
+        canvas.drawCustomShape(with: Triangle2)
+        
+    }
+}
 
-
-
-canvas.highPerformance = false
+// Write Fivio Foreign
+canvas.lineColor = darkBlue
+canvas.drawText(message: "Fivio Foreign", at: Point(x: 25, y: 437), size: 10, kerning: 5)
 
 
 
