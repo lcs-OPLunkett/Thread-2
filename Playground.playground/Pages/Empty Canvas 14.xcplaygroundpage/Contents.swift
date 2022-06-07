d14638bdd7c09e5b0046dccc41532f2e9655160b
Owen Plunkett <oplunkett@lcs.on.ca>
2022-06-07 12:23:27 -0400

    Started my Triangle design.

--- Playground.playground/Pages/Empty Canvas 14.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Empty Canvas 14.xcplaygroundpage/Contents.swift
@@ -1,11 +1,11 @@
 //: [Previous](@previous) / [Next](@next)
 /*:
-## Canvas size
+ ## Canvas size
  
  Set the size of your desired canvas by adjusting the constants on lines 7 and 8.
  */
-let preferredWidth = 600
-let preferredHeight = 600
+let preferredWidth = 400
+let preferredHeight = 400
 /*:
  ## Required code
  
@@ -17,14 +17,12 @@ import Cocoa
 import PlaygroundSupport
 import CanvasGraphics
 
+
 // Create canvas
 let canvas = Canvas(width: preferredWidth, height: preferredHeight)
 
-// Create a turtle that can draw upon the canvas
-let turtle = Tortoise(drawingUpon: canvas)
+let t =  Tortoise(drawingUpon: canvas)
 
-// Create a pen that can draw upon the canvas
-let p = Pen(drawingUpon: canvas)
 
 // Show the canvas in the playground's live view
 PlaygroundPage.current.liveView = canvas
@@ -40,39 +38,265 @@ PlaygroundPage.current.liveView = canvas
  
  */
 
-// Move the origin from the bottom-left corner of the canvas to it's centre point
-canvas.translate(to: Point(x: canvas.width / 2,
-                           y: canvas.height / 2))
+
 
 // Show a grid
 canvas.drawAxes(withScale: true, by: 20, color: .black)
+
+
 
 /*:
  ## Add your code
  
  Beginning on line 61, you can add your own code.
-  
+ 
  [Documentation](http://russellgordon.ca/CanvasGraphics/Documentation/) is available.
-
+ 
  */
+canvas.highPerformance = true
+//Set functions
+let scale = 20
+let diagnol = Int(sqrt(2)*Double(scale))
 
-// Begin writing your code below (you can remove the examples shown)
+//Get pen into postion
 
-// Draw a circle, using the canvas object directly
-canvas.drawEllipse(at: Point(x: 100, y: 100), width: 25, height: 25)
+t.penUp()
+t.forward(steps: 1*scale)
+t.left(by: 90)
+t.forward(steps: 22*scale)
+t.right(by: 90)
 
-// Draw a vertical line, up and to the left
-p.drawTo(dx: -25, dy: 50)
+// Draw Trianle
 
-// Go back to origin
-p.goToOrigin()
+func drawTriangle (){
+    t.penDown()
+    t.forward(steps: 4*diagnol)
+    t.left(by: 135)
+    t.forward(steps: 3*diagnol)
+    t.left(by: 90)
+    t.forward(steps: 3*diagnol)
+    t.left(by: 135)
+}
 
-// Change the pen color
-p.penColor = .red
+func moveBackToStart1(){
+    t.penUp()
+    t.backward(steps:scale*20)
+    t.right(by: 90)
+    t.forward(steps: scale*1)
+    t.left(by: 90)
+    t.penDown()
+}
 
-// Draw a curve, down and to the right
-p.addArc(radius: 50, angle: -45)
+func moveBackToStart2(){
+    t.penUp()
+    t.backward(steps:scale*26)
+    t.right(by: 90)
+    t.forward(steps: scale*1)
+    t.left(by: 90)
+    t.penDown()
+    
+}
 
+for _ in 1 ... 4{
+    drawTriangle()
+    t.penUp()
+    t.forward(steps: scale*6)
+    t.penDown()
+    
+}
+
+moveBackToStart1()
+
+
+for _ in 1 ... 4{
+    drawTriangle()
+    t.penUp()
+    t.forward(steps: scale*6)
+    t.penDown()
+    
+}
+
+moveBackToStart2()
+
+for _ in 1 ... 4{
+    drawTriangle()
+    t.penUp()
+    t.forward(steps: scale*6)
+    t.penDown()
+    
+}
+
+moveBackToStart1()
+
+for _ in 1 ... 4{
+    drawTriangle()
+    t.penUp()
+    t.forward(steps: scale*6)
+    t.penDown()
+    
+}
+
+moveBackToStart2()
+
+for _ in 1 ... 4{
+    drawTriangle()
+    t.penUp()
+    t.forward(steps: scale*6)
+    t.penDown()
+    
+}
+
+moveBackToStart1()
+
+for _ in 1 ... 4{
+    drawTriangle()
+    t.penUp()
+    t.forward(steps: scale*6)
+    t.penDown()
+    
+}
+
+moveBackToStart2()
+
+for _ in 1 ... 4{
+    drawTriangle()
+    t.penUp()
+    t.forward(steps: scale*6)
+    t.penDown()
+    
+}
+
+moveBackToStart1()
+
+for _ in 1 ... 4{
+    drawTriangle()
+    t.penUp()
+    t.forward(steps: scale*6)
+    t.penDown()
+    
+}
+
+moveBackToStart2()
+
+for _ in 1 ... 4{
+    drawTriangle()
+    t.penUp()
+    t.forward(steps: scale*6)
+    t.penDown()
+    
+}
+
+moveBackToStart1()
+
+for _ in 1 ... 4{
+    drawTriangle()
+    t.penUp()
+    t.forward(steps: scale*6)
+    t.penDown()
+    
+}
+
+moveBackToStart2()
+
+for _ in 1 ... 4{
+    drawTriangle()
+    t.penUp()
+    t.forward(steps: scale*6)
+    t.penDown()
+    
+}
+
+moveBackToStart1()
+
+for _ in 1 ... 4{
+    drawTriangle()
+    t.penUp()
+    t.forward(steps: scale*6)
+    t.penDown()
+    
+}
+
+moveBackToStart2()
+
+for _ in 1 ... 4{
+    drawTriangle()
+    t.penUp()
+    t.forward(steps: scale*6)
+    t.penDown()
+    
+}
+
+moveBackToStart1()
+
+for _ in 1 ... 4{
+    drawTriangle()
+    t.penUp()
+    t.forward(steps: scale*6)
+    t.penDown()
+    
+}
+
+moveBackToStart2()
+
+for _ in 1 ... 4{
+    drawTriangle()
+    t.penUp()
+    t.forward(steps: scale*6)
+    t.penDown()
+    
+}
+
+moveBackToStart1()
+
+for _ in 1 ... 4{
+    drawTriangle()
+    t.penUp()
+    t.forward(steps: scale*6)
+    t.penDown()
+    
+}
+
+moveBackToStart2()
+
+for _ in 1 ... 4{
+    drawTriangle()
+    t.penUp()
+    t.forward(steps: scale*6)
+    t.penDown()
+    
+}
+
+moveBackToStart1()
+
+for _ in 1 ... 4{
+    drawTriangle()
+    t.penUp()
+    t.forward(steps: scale*6)
+    t.penDown()
+    
+}
+
+moveBackToStart2()
+
+for _ in 1 ... 4{
+    drawTriangle()
+    t.penUp()
+    t.forward(steps: scale*6)
+    t.penDown()
+    
+}
+
+moveBackToStart1()
+
+for _ in 1 ... 4{
+    drawTriangle()
+    t.penUp()
+    t.forward(steps: scale*6)
+    t.penDown()
+    
+}
+
+canvas.highPerformance = false
 /*:
  ## Show the Live View
  Don't see any results?
@@ -80,7 +304,7 @@ p.addArc(radius: 50, angle: -45)
  Remember to show the Live View (1 then 2):
  
  ![timeline](timeline.png "Timeline")
-
+ 
  ## Use source control
  To keep your work organized, receive feedback, and earn a high grade in this course, regular use of source control is a must.
  
